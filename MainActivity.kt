608dbb3f965691fcea1c694e434f993f853303e5
liyaprog <optimashymkent@gmail.com>
2025-06-09 22:09:13 +0500

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,11 @@
+package com.lia
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Точка входа: здесь можно подключать Лию офлайн
+    }
+}
